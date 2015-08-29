cf01a1ef2d4709f44e61802f9c40f8f88d45f2f0
Harshada Kurahatti <harshada.kurahatti@gmail.com>
2015-08-29 14:53:52 -0700

    Commit 2:
    Added mouseenter and mouseout events to app.js

---
+++ js/app.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+	$(".ryu").mouseenter(function(){
+		$(".ryu-still").hide();
+		$(".ryu-ready").show();
+	})
+
+	.mouseleave(function(){
+		$(".ryu-ready").hide();
+		$(".ryu-still").show();
+	});
+    // console.log( banana );
+    // alert( banana );
+});
