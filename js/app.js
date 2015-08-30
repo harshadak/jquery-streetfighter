8a7c46674b4f1e571422e39bb07cb9cf693340f8
Harshada Kurahatti <harshada.kurahatti@gmail.com>
2015-08-29 17:39:20 -0700

    Commit 3:
    Created the Ryu throwing position by adding mousedown and mouseup event handlers.

--- js/app.js
+++ js/app.js
@@ -7,6 +7,20 @@ $( document ).ready(function() {
 	.mouseleave(function(){
 		$(".ryu-ready").hide();
 		$(".ryu-still").show();
+	})
+
+	.mousedown(function(){
+		$(".ryu-ready").hide();
+		$(".ryu-throwing").show();
+		$(".hadouken").show();
+		// play hadouken sound
+		// show hadouken and animate it to the right of the screen
+	})
+
+	.mouseup(function(){
+		$(".ryu-ready").show();
+		$(".ryu-throwing").hide();
+		// ryu goes back to his ready position
 	});
     // console.log( banana );
     // alert( banana );
