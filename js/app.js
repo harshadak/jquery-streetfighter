68511858f43c66bc233e4ebeea58590eb48d563e
Harshada Kurahatti <harshada.kurahatti@gmail.com>
2015-08-30 15:41:40 -0700

    Commit 4:
    Added Hadouken throwing and implemented the Hadouken sound.

--- js/app.js
+++ js/app.js
@@ -1,27 +1,42 @@
 $( document ).ready(function() {
+
+	// Ryu in ready position
 	$(".ryu").mouseenter(function(){
 		$(".ryu-still").hide();
 		$(".ryu-ready").show();
 	})
 
+	// Ryu in still position
 	.mouseleave(function(){
 		$(".ryu-ready").hide();
 		$(".ryu-still").show();
 	})
 
+	// Ryu throws Hadouken and makes sound
 	.mousedown(function(){
+		playHadouken();
 		$(".ryu-ready").hide();
 		$(".ryu-throwing").show();
-		$(".hadouken").show();
-		// play hadouken sound
-		// show hadouken and animate it to the right of the screen
+		$(".hadouken").finish().show().animate(
+			{"left": "1020px"}, 500,
+			function(){
+				$(this).hide();
+				$(this).css("left", "520px");
+			}
+		);	
 	})
 
+	// Ryu in ready position
 	.mouseup(function(){
 		$(".ryu-ready").show();
 		$(".ryu-throwing").hide();
-		// ryu goes back to his ready position
 	});
-    // console.log( banana );
-    // alert( banana );
 });
+
+
+// Function to play Hadouken sound
+function playHadouken() {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
