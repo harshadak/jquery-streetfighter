42a59830536b4e08008b2ee8e388d9f8a2ea6bd0
Harshada Kurahatti <harshada.kurahatti@gmail.com>
2016-02-08 21:01:24 -0800

    Made some changes in app.js Moved the code for div show after all the div hides.

--- js/app.js
+++ js/app.js
@@ -1,66 +1,67 @@
-$( document ).ready(function() {
+$(document).ready(function () {
 
-	// Ryu in ready position
-	$(".ryu").mouseenter(function() {
-		$(".ryu-still").hide();
-		$(".ryu-ready").show();
-		$(".ryu-throwing").hide();
-		$(".ryu-cool").hide();
-	})
+    // Ryu in ready position
+    $(".ryu").mouseenter(function () {
+        $(".ryu-still").hide();
+        $(".ryu-throwing").hide();
+        $(".ryu-cool").hide();
+        $(".ryu-ready").show();
+    })
 
-	// Ryu in still position
-	.mouseleave(function() {
-		$(".ryu-ready").hide();
-		$(".ryu-still").show();
-		$(".ryu-throwing").hide();
-		$(".ryu-cool").hide();
-	})
+    // Ryu in still position
+    .mouseleave(function () {
+        $(".ryu-ready").hide();
+        $(".ryu-throwing").hide();
+        $(".ryu-cool").hide();
+        $(".ryu-still").show();
+    })
 
-	// Ryu throws Hadouken and makes sound
-	.mousedown(function() {
-		playHadouken();
-		$(".ryu-ready").hide();
-		$(".ryu-throwing").show();
-		$(".ryu-still").hide();
-		$(".ryu-cool").hide();
-		$(".hadouken").finish().show().animate(
-			{"left": "1020px"}, 500,
-			function() {
-				$(this).hide();
-				$(this).css("left", "520px");
-			}
-		);	
-	})
+    // Ryu throws Hadouken and makes sound
+    .mousedown(function () {
+        playHadouken();
+        $(".ryu-ready").hide();
+        $(".ryu-still").hide();
+        $(".ryu-cool").hide();
+        $(".ryu-throwing").show();
+        $(".hadouken").finish().show().animate({
+                "left": "1020px"
+            }, 500,
+            function () {
+                $(this).hide();
+                $(this).css("left", "520px");
+            }
+        );
+    })
 
-	// Ryu in ready position
-	.mouseup(function() {
-		$(".ryu-ready").show();
-		$(".ryu-throwing").hide();
-		$(".ryu-still").hide();
-		$(".ryu-cool").hide();
-	});
+    // Ryu in ready position
+    .mouseup(function () {
+        $(".ryu-throwing").hide();
+        $(".ryu-still").hide();
+        $(".ryu-cool").hide();
+        $(".ryu-ready").show();
+    });
 
-	// Ryu in cool position
-	$(document).keydown(function(e) {
-		if (e.keyCode == 88) {
-			playMusic();
-			$(".ryu-ready").hide();
-			$(".ryu-cool").show();
-			$(".ryu-still").hide();
-			$(".ryu-throwing").hide();
-		}
-	})
+    // Ryu in cool position
+    $(document).keydown(function (e) {
+        if (e.keyCode == 88) {
+            playMusic();
+            $(".ryu-ready").hide();
+            $(".ryu-still").hide();
+            $(".ryu-throwing").hide();
+            $(".ryu-cool").show();
+        }
+    })
 
-	.keyup(function(e) {
-		if (e.keyCode == 88) {
-			$("#cool-music")[0].pause();
-			$("#cool-music")[0].load();
-			$(".ryu-cool").hide();
-			$(".ryu-still").show();
-			$(".ryu-throwing").hide();
-			$(".ryu-ready").hide();
-		}
-	});	
+    .keyup(function (e) {
+        if (e.keyCode == 88) {
+            $("#cool-music")[0].pause();
+            $("#cool-music")[0].load();
+            $(".ryu-cool").hide();
+            $(".ryu-throwing").hide();
+            $(".ryu-ready").hide();
+            $(".ryu-still").show();
+        }
+    });
 });
 
 
@@ -68,14 +69,14 @@ $( document ).ready(function() {
 
 // Function to play Hadouken sound
 function playHadouken() {
-  $("#hadouken-sound")[0].volume = 0.5;
-  $("#hadouken-sound")[0].load();
-  $("#hadouken-sound")[0].play();
+    $("#hadouken-sound")[0].volume = 0.5;
+    $("#hadouken-sound")[0].load();
+    $("#hadouken-sound")[0].play();
 }
 
 // Function to play cool music
 function playMusic() {
-	$("#cool-music")[0].volume = 0.5;
-	//$("#cool-music")[0].load();
-  	$("#cool-music")[0].play();
+    $("#cool-music")[0].volume = 0.5;
+    //$("#cool-music")[0].load();
+    $("#cool-music")[0].play();
 }
